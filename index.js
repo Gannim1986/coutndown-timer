eb721f1d5fab742a6d9693332099949193c9e5e1
Gannim1986 <36219943+Gannim1986@users.noreply.github.com>
2022-12-30 13:40:28 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const newYears = '1 Jan 2023';
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+function countdown() {
+    const newYearDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+    
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24 % 60;
+    const mins = Math.floor(totalSeconds / 60) %60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    
+    // console.log(days, hours, mins);
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+ function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+ }
+ function myFunc() {
+    if (newYearDate === newYears)
+    return(`Happy new year ${newYearDate}`) 
+
+ }
+
+countdown();
+
+setInterval(countdown, 1000);
+
